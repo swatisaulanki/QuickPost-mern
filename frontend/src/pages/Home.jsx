da0e87ba5346d8aad900f3cbdc2f66be14f09ac7
swatisaulanki <swatisaulanki@gmail.com>
2025-07-04 12:42:32 +0530

    code refactor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="p-6 md:h-[680px] bg-custom-color3">
+    <div className="p-6 min-h-screen bg-custom-color3">
       <h2 className="text-2xl font-bold text-white mb-6">📢 Latest Posts</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
